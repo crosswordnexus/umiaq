2ea1c783b06b7a325310c4c5c00695b4c0964c97
Alex Boisvert <boisvert42@gmail.com>
2025-06-24 19:30:05 -0700

    Update umiaq.py

--- umiaq.py
+++ umiaq.py
@@ -428,6 +428,7 @@ class MyArgs:
         self.input = _input
         self.debug = False
         self.num_results = NUM_RESULTS
+        self.minscore = 0
 
 def solve_equation(_input, num_results=NUM_RESULTS,
                 max_word_length=MAX_WORD_LENGTH, return_json=False, min_score=0):
